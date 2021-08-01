67543d1da4ec60873896b2b2ff27c7a47b41f2e7
O-luwanifemi <66732138+O-luwanifemi@users.noreply.github.com>
2021-08-01 17:19:36 +0100

    Made adjustment task 2

--- task_two.js
+++ task_two.js
@@ -1,4 +1,11 @@
-//// Q2
+// QUESTION: Create a hashmap (or a simple object in PHP) that contains the elementary functions of addition, subtraction, multiplication, and division.
+// Take into account that the division must not allow 0 dividend
+// The sum function allows an array as an input parameter and adds all its elements.
+// The subtraction function allows an array as an input parameter and subtracts all its elements.
+// Multiplication Function - Ditto
+// The division function accepts two parameters: a and b.
+
+
 const mathMap = new Map();
 
 // a
